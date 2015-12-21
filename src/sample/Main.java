fa484d4b3ad3ec9a258aec2e00d68583536bff38
Brendon Butler <bbutler452@gmail.com>
2015-12-21 13:07:40 -0500

    Added light switch images modified application size to match files' dimensions

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/switch.fxml"));
         primaryStage.setTitle("Light Switch");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 384, 595));
         primaryStage.show();
     }
 
